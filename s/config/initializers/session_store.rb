19b00a944668a16eb6158e1bad9a187449799ed3
ltwheeler <ltwheeler@ucdavis.edu>
2013-06-03 15:57:34 -0700

    installed twitter bootstrap and begun work on a navbar

---
+++ s/config/initializers/session_store.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_s_session',
+  :secret      => 'f476ea9291d02b0cd9938616210837b18c29473db4b75c22738a0464562a7acfcdb989cbf429bd3b75f6fe4ad8b47c96c59bfe39008ffac644bf88b0e0e69ea5'
+}
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# ActionController::Base.session_store = :active_record_store
