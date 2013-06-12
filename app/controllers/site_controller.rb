3d4f33089c5889c516f87f8ccc9059b1906fd2a0
ltwheeler <ltwheeler@ucdavis.edu>
2013-06-12 16:05:07 -0700

    refactoring on site controller

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,7 +2,7 @@ class SiteController < ApplicationController
 
 	def index
 		@apps = Application.all
-		@favorites = Favorite.find(:all, :conditions => ["loginid=?", @current_user.loginid], :order => 'position' )
+		@favorites = Favorite.find_all_by_loginid(@current_user.loginid, :order => 'position')
 	end
 	
 end
